3a828d4c5a65b4e6e24547812fc79f36d19ffec7
GerstnerJuanI <43797469+GerstnerJuanI@users.noreply.github.com>
2022-08-21 01:06:42 -0300

    se creo la función de encriptación.
    se debe fusionar con la de mostrar y ocultar imagen.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,51 @@
-window.addEventListener('load', init, false);
-function init() {
     let img = document.querySelector('#img-texto');
     let textarea = document.querySelector('#textodesc');
-    img.style.display= 'block';
+    let boton1 = document.querySelector('#boton1');
+    let textoSinEnc = document.getElementById('inputEncrip');
+    let textodesc = document.getElementById('textodesc');
+window.addEventListener('load', init, false);
+
+function init() {
+    img.style.display = 'block';
     textarea.style.display = 'none';
-    let boton = document.querySelector('#boton1');
-    boton.addEventListener('click', function (e) {
-        if(img.style.display === 'block' && textarea.style.display === 'none'){
+
+    boton1.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (img.style.display === 'block' && textarea.style.display === 'none') {
             img.style.display = 'none';
             textarea.style.display = 'block';
-        }else{
-            img.style.display= 'block';
-            textarea.style.display= 'none';
+        } else {
+            img.style.display = 'block';
+            textarea.style.display = 'none';
         }
     }, false);
+
+    boton1.addEventListener('click',encriptar,false);
+};
+
+function mostrarImagen() {
+    
+}
+function encriptar(e) {
+    e.preventDefault();
+    textodesc.value = '';
+    let textoAEncriptar = textoSinEnc.value;
+    let expReg = /[^a-z ñ]+/;
+    const regla = [
+        {letra:'a',codigo:'ai'},
+        {letra:'e',codigo:'enter'},
+        {letra:'i',codigo:'imes'},
+        {letra:'o',codigo:'ober'},
+        {letra:'u',codigo:'ufat'}
+    ];
+    if (!expReg.test(textoAEncriptar)) { // con esto verificamos que solo se introduzcan letras minusculas 
+        let textoEncriptado = textoAEncriptar;
+        for (const element in regla) {
+            textoEncriptado = textoEncriptado.replaceAll(regla[element].letra,regla[element].codigo);
+        }
+        textodesc.value = textoEncriptado;
+    } else {
+        textodesc.value = 'SOLO SE PERMITEN LETRAS MINUSCULAS! Intentelo nuevamente.';
+        return;
+    }
 }
